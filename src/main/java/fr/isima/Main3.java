1b65175b7c4783c774ffdf01dacc851a3c28836e
jH@CKtheRipper <passerat@isima.fr>
2013-03-31 20:39:11 +0200

    Measure time spent in getTaskID

---
+++ src/main/java/fr/isima/Main3.java
@@ -0,0 +1,49 @@
+import fr.isima.random.*;
+import fr.isima.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author jopasserat
+ */
+public class Main3 extends RandomSafeRunnable {
+
+  @Override
+  public void run() {
+
+  }
+
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] argv) {
+    Main3 task = new Main3();
+
+    final int loopsToPerform = Integer.parseInt(argv[0]);
+    final int callsToPerform = Integer.parseInt(argv[1]);
+
+    long totalTime = 0;
+
+    // averaging loop
+    for (int i = 0; i < loopsToPerform; ++i) {
+      // start timing
+      //long startTime = System.nanoTime();
+      for (int j = 0; j < callsToPerform; ++j) {
+	task.getTaskId();
+      }
+      // stop timing and sum
+      //long endTime = System.nanoTime();
+
+      //totalTime += (endTime - startTime);
+    }
+
+/*
+    System.out.println ( "(" + loopsToPerform + " x ) " + callsToPerform +
+	" calls to RandomSafeRunnable.getTaskId() performed in " + 
+	totalTime / loopsToPerform + " ns" );
+*/
+  }
+}
+
